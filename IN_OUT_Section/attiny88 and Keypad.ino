8ec12d205e34284916d05fc2c7317239e17a61ee
Syj152al2023 <147919829+Syj152al2023@users.noreply.github.com>
2023-12-04 08:28:32 -0600

    Create attiny88 and Keypad.ino

---
+++ IN_OUT_Section/attiny88 and Keypad.ino
@@ -0,0 +1,29 @@
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+const byte ROW_NUM = 4;
+const byte COL_NUM = 4;
+LiquidCrystal_I2C lcd(0x27,16, 2);
+char keys[ROW_NUM][COL_NUM] = {
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*','0','#','D'}  };
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};
+byte pin_column[COL_NUM] = {5, 4, 3, 2};
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COL_NUM );
+void setup(){
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
+void loop(){
+  char key = keypad.getKey();
+  if (key){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Key Pressed: ");
+    lcd.setCursor(0,1);
+    lcd.print(key);
+  }
+}
