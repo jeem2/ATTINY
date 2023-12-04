e50be7f9813daefd732572d5744ce78f5c112f75
Syj152al2023 <147919829+Syj152al2023@users.noreply.github.com>
2023-12-04 08:18:06 -0600

    Update simple counter.ino

--- IN_OUT_Section/simple counter.ino
+++ IN_OUT_Section/simple counter.ino
@@ -1,1 +1,36 @@
+#include <TinyWireM.h>
+#include <TM1637Display.h>
 
+const int CLK = 0;
+const int DIO = 1;
+TM1637Display display(CLK, DIO);
+
+const int BUTTON_PIN_INC = 2;
+const int BUTTON_PIN_RESET = 4;
+
+int counter = 0;
+int buttonStateInc = 0;
+int buttonStateReset = 0;
+
+void setup() {
+  display.setBrightness(7);
+  pinMode(BUTTON_PIN_INC, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_RESET, INPUT_PULLUP);
+}
+
+void loop() {
+  buttonStateInc = digitalRead(BUTTON_PIN_INC);
+  buttonStateReset = digitalRead(BUTTON_PIN_RESET);
+
+  if (buttonStateInc == LOW) {
+    counter++;
+    display.showNumberDec(counter);
+    delay(250);
+  }
+
+  if (buttonStateReset == LOW) {
+    counter = 0;
+    display.showNumberDec(counter);
+    delay(250);
+  }
+}
